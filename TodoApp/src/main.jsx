fbcf628a12534753ffffbbfcca6faa519a218cf7
Supreme555 <isabekov.abylay8@mail.ru>
2024-03-15 10:22:28 +0600

    3

--- TodoApp/src/main.jsx
+++ TodoApp/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//const element = React.createElement('hi', null, 'Hello React World');
+
+const element = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Install React</li>
+      <li>Study React</li>
+      <li>Use React</li>
+      <li>Build React App</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(element,
+  document.getElementById('root'));
