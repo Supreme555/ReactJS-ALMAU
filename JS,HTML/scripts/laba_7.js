9ec3adcb1d944ee1e2bd6fbaf637ba95dab106fe
Supreme555 <isabekov.abylay8@mail.ru>
2024-03-15 10:11:09 +0600

    1

--- JS,HTML/scripts/laba_7.js
+++ JS,HTML/scripts/laba_7.js
@@ -14,28 +14,6 @@
 // printForecast(data1);
 // printForecast(data2);
 
-// // Task 2
-// function generateRandomHexColor() {
-//     // Generate a random number between 0 and 16777215
-//     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-//     // Ensure the color has six digits by padding with zeros if necessary
-//     return '#' + ('000000' + randomColor).slice(-6);
-// }
-// document.querySelector('.again').addEventListener('click', function() {
-//     score = 20;
-//     secretNumber = Math.trunc(Math.random() * 20) + 1;
-//     document.querySelector('.message').textContent = 'Start guessing...';
-//     document.querySelector('.number').textContent = '?';
-//     document.querySelector('.score').textContent = secretNumber;
-//     document.querySelector('.guess').value = '';
-
-//     document.querySelector('body').style.backgroundColor = generateRandomHexColor();
-//     document.querySelector('.number').style.width = '15rem';
-// });
-
-//Task 2
-
-
 // Task 3
 HigthScore = 0;
 score = 0
