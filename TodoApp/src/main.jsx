30fd538bffe44c53199b74eb633c0799e54052ef
Supreme555 <isabekov.abylay8@mail.ru>
2024-04-05 10:56:28 +0600

    14

--- TodoApp/src/main.jsx
+++ TodoApp/src/main.jsx
@@ -1,31 +1,45 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
+import { createRoot } from 'react-dom/client';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './main.css';
 
 const App = () => {
 
   const todoData = [
     { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Implement React Application', important: true, id: 2 },
+    { label: 'React Application', important: true, id: 2 },
     { label: 'Make notes from your stud', important: false, id: 3 },
     { label: 'Do not drink alchohol', important: false, id: 4 },
   ];
 
 
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
       <TodoList todos={todoData} />
-      <ItemStatusFilter />
     </div>
   );
 }
 
 // const element = <App />;
 
-ReactDOM.render(<App />,
-  document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+// const todoData = [
+//     { label: 'Drink Coffee', important: false,  id: 1},
+//     { label: 'Implement React Application', important: true, id: 2 },
+//     { label: 'Make notes from your stud', important: false, id: 3 },
+//     { label: 'Do not drink alchohol', important: false, id: 4 },
+// ];
