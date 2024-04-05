5efb1310411330f9f8b6294328012dd028e86a5c
Supreme555 <isabekov.abylay8@mail.ru>
2024-04-05 10:28:34 +0600

    6

--- TodoApp/src/main.jsx
+++ TodoApp/src/main.jsx
@@ -1,34 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-
-const TodoList = () => {
-  const items = ['Install React', 'Study Reac', 'Use React', 'Build React App'];
-  return (
-    <ul>
-      <li>{items[0]}</li>
-      <li>{items[1]}</li>
-      <li>{items[2]}</li>
-      <li>{items[3]}</li>
-    </ul>
-  );
-};
-
-const AppHeader = () => {
-  return <h1>My Todo List</h1>;
-};
-
-const SearchPanel = () => {
-  const searchText = 'Type here to search';
-  const searchStyle = {
-    fontSize: '20px'
-  };
-
-  // return <input placeholder="search" />;
-  return <input
-    style={searchStyle}
-    placeholder={searchText}
-    disabled={true} />;
-}
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
 const App = () => {
   const value = '<script>alert ("")</script>';
@@ -38,8 +12,6 @@ const App = () => {
 
   return (
     <div>
-      {/*{ isLoggedIn ? null : loginBox }*/}
-      {value}
       <AppHeader />
       <SearchPanel />
       <TodoList />
