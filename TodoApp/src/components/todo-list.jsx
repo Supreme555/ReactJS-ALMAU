a76e945f30dad10dd0525322723736e3930f0395
Supreme555 <isabekov.abylay8@mail.ru>
2024-04-05 10:41:29 +0600

    11

--- TodoApp/src/components/todo-list.jsx
+++ TodoApp/src/components/todo-list.jsx
@@ -4,16 +4,17 @@ import TodoListItem from "./todo-list-item";
 const TodoList = ({ todos }) => {
 
     const elements = todos.map((item) => {
+        const { id, ...itemProps } = item;
         return (
-            <li>
-                <TodoListItem {...item } />
+            <li key={item.id} >
+                <TodoListItem {...itemProps} />
             </li>
         );
     });
 
     return (
         <ul>
-           { elements }
+            {elements}
         </ul>
     );
 };
