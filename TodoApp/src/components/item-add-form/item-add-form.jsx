8f8f7b54964ab636ac296624f382d5654f5fb58b
Supreme555 <isabekov.abylay8@mail.ru>
2024-04-10 17:10:01 +0600

    1

--- TodoApp/src/components/item-add-form/item-add-form.jsx
+++ TodoApp/src/components/item-add-form/item-add-form.jsx
@@ -1,74 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import './item-add-form.css';
 
 
-export default ItemAddForm = (addItem) => {
+export default class ItemAddForm extends Component {
 
-    const [state, setState] = useState(initialState)
-    
-
-    const onLabelChange = (e) => {
-        setState(e.target.value)
+    state = {
+        label: ''
     };
-    const onSubmit = (e) => {
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+    onSubmit = (e) => {
         e.preventDefault();
         this.props.onItemAdded(this.state.label);
         this.setState({
             label: ''
         });
     };
-    return (
-        <form className={"item-add-form d-flex"}
-            onSubmit={onSubmit}>
+    render() {
+        return (
+            <form className={"item-add-form d-flex"}
+                onSubmit={this.onSubmit}>
 
-            <input type={"text"}
-                className={"form-control"}
-                onChange={onLabelChange}
-                placeholder={"What need to be done"}
-                value={state} />
-            <button
-                className={"btn btn-outline-secondary"} >
-                Add Item
+                <input type={"text"}
+                    className={"form-control"}
+                    onChange={this.onLabelChange}
+                    placeholder={"What need to be done"}
+                    value={this.state.label} />
+                <button
+                    className={"btn btn-outline-secondary"} >
+                    Add Item
 
-            </button>
-        </form>
-    )
+                </button>
+            </form>
+        )
+    }
 }
-
-// export default class ItemAddForm extends Component {
-
-//     state = {
-//         label: ''
-//     };
-
-//     onLabelChange = (e) => {
-//         this.setState({
-//             label: e.target.value
-//         });
-//     };
-//     onSubmit = (e) => {
-//         e.preventDefault();
-//         this.props.onItemAdded(this.state.label);
-//         this.setState({
-//             label: ''
-//         });
-//     };
-//     render() {
-//         return (
-//             <form className={"item-add-form d-flex"}
-//                 onSubmit={this.onSubmit}>
-
-//                 <input type={"text"}
-//                     className={"form-control"}
-//                     onChange={this.onLabelChange}
-//                     placeholder={"What need to be done"}
-//                     value={this.state.label} />
-//                 <button
-//                     className={"btn btn-outline-secondary"} >
-//                     Add Item
-
-//                 </button>
-//             </form>
-//         )
-//     }
-// }
