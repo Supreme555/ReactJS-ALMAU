8d4d19c611949418640cdf9040c7f389633c7181
Supreme555 <isabekov.abylay8@mail.ru>
2024-04-25 22:55:05 +0600

    I have done refactoring

--- react-starDB/src/components/sw-components/index.js
+++ react-starDB/src/components/sw-components/index.js
@@ -1,22 +1,49 @@
-import {
-    PersonList,
-    StarshipList,
-    PlanetList
-} from './item-lists'
+import React from 'react';
+import ItemList from "../item-list";
+import {withData, withSwapiService, withChildFunction, compose} from "../hoc-helpers";
 
-import {
-    PersonDetails,
-    StarshipDetails,
-    PlanetDetails
-} from './details'
+
+
+
+const renderName = ({name}) => <span>{name}</span>;
+const renderModelAndName =
+    ({model, name}) => <span>{name} ({model})</span>;
+
+const mapPersonMethodsToProps = (swapiService) => {
+    return { getData: swapiService.getAllPeople }
+};
+const mapPlanetMethodsToProps = (swapiService) => {
+    return { getData: swapiService.getAllPlanets }
+};
+const mapStarshipMethodsToProps = (swapiService) => {
+    return { getData: swapiService.getAllStarships }
+};
+
+
+const PersonList = compose(
+    withSwapiService(mapPersonMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
+
+
+const PlanetList = compose(
+    withSwapiService(mapPlanetMethodsToProps),
+    withData,
+    withChildFunction(renderName)
+)(ItemList);
+
+
+const StarshipList = compose(
+    withSwapiService(mapStarshipMethodsToProps),
+    withData,
+    withChildFunction(renderModelAndName)
+)(ItemList);
 
 
 
 export {
     PersonList,
-    StarshipList,
     PlanetList,
-    PersonDetails,
-    StarshipDetails,
-    PlanetDetails
+    StarshipList
 }
